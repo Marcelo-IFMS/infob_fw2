31654c0ff3f758a002ef34ce32a1dd6ad39fd648
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-09-26 07:40:39 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const app = require('./config/server')
 const port = require('./config/port')
-const client = require('./config/db')
+
 
 
 app.listen(port)
--- route/db.js
+++ route/db.js
@@ -1,13 +1,15 @@
-// app.get('/db', async (req, res) => {
-//   try {
-//     await client.connect();
-//     await client.db("admin").command({ ping: 1 });
-//     const movies = await client.db('sample_mflix').collection('users').find({ name: /^Ma/ }).toArray();
-//     res.json({ msg: "Arquivo db.js executado", resultado: movies });
-
-//   } finally {
-//     //encerra ocorrendo erro
-//     await client.close();
-//   }
-
-// })
+const client = require('../config/db')
+module.exports = function (app) {
+    app.get('/db', async (req, res) => {
+        try {
+            await client.connect();
+            const movies = await client.db('sample_mflix').collection('users').find({ name: /^Ma/ }).toArray();
+            res.json({ msg: "Arquivo db.js executado", resultado: movies });
+            
+        } finally {
+            //encerra ocorrendo erro
+            await client.close();
+        }
+        
+    })
+}
